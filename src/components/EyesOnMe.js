d485cdb12233a978053171c118def4767711a896
George-Okumu <gokumu12@gmail.com>
2022-04-11 15:18:59 +0300

    assigment complete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe(){
+    function focus(){
+        return console.log("Good!");
+    }
+    function blur(){
+        return console.log("Hey! Eyes on me!");
+    }
+
+
+
+    return (
+        <>
+        <button type="submit" onFocus={focus} onBlur={blur}>Eyes on me</button>
+        </>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
 // Code Keypad Component Here
+import React from "react";
+
+function Keypad(){
+    function changeMe(){
+        return console.log("Entering password...");
+    }
+    return (
+        <>
+            <input type="password" onChange={changeMe} placeholder="Enter Password" required/>
+        </>
+    )
+}
+
+export default Keypad;
